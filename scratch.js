d0dfb641b8ee01344bc9bceccd2739a0960355ca
selegris1-cmyk <selegris1@gmail.com>
2026-01-29 21:15:30 +0100

    Update scratch.js

--- scratch.js
+++ scratch.js
@@ -1,3 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+
+  const form = document.getElementById("convertForm");
+  const metersInput = document.getElementById("meters");
+  const unitSelect = document.getElementById("unit");
+
+
+  form.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+    const meters = Number(metersInput.value);
+    const factor = Number(unitSelect.value);
+    const unitName = unitSelect.options[unitSelect.selectedIndex].dataset.name;
+
+    let result = meters * factor;
+    alert(meters + " meters are exactly " + result + " " + unitName + " long");
+
+  });
+});
+console.log("Hello from embedded JavaScript");
 console.log("hello world")
 alert("hello world")
 const value = 1.0936132983
